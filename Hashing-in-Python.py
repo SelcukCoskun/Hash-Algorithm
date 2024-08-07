d2d2b113f9d198b219de56e3624c4e1683c5d208
SelcukCoskun <150424355+SelcukCoskun@users.noreply.github.com>
2024-08-07 19:34:08 +0300

    Create Hashing-in-Python.py

---
+++ Hashing-in-Python.py
@@ -0,0 +1,35 @@
+import hashlib
+
+while True:
+ text=input("Enter the text you want to hash =")
+
+ text_utf=text.encode('utf-8')
+
+ choice=input("Which hash algorithm do you want to use?/md-5/sha-1/sha-256/sha-512=")
+
+ if choice =="md5":
+      hash=hashlib.md5(text_utf)
+      hash2=hash.hexdigest()
+      print(hash2)
+      break
+
+ elif choice=="sha-1":
+     hash=hashlib.sha1(text_utf)
+     hash2=hash.hexdigest()
+     print(hash2)
+     break
+
+ elif choice=="sha-256":
+     hash=hashlib.sha256(text_utf)
+     hash2=hash.hexdigest()
+     print(hash2)
+     break
+
+ elif choice=="sha-512":
+     hash=hashlib.sha512(text_utf)
+     hash2=hash.hexdigest()
+     print(hash2)
+     break
+
+ else:
+     print("Enter a valid hash algorithm")
